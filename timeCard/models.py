19122e4f44ac53d9e1485726551688f5421fb772
Dmitriy Grechkin <dgrechkin92@gmail.com>
2019-09-28 20:13:38 -0400

    report and timeCard

--- timeCard/models.py
+++ timeCard/models.py
@@ -5,12 +5,10 @@ from doctor.models import Doctor
 
 class TimeCard(models.Model):
     file_number = models.ForeignKey(Doctor, on_delete=models.CASCADE)  # Connected with Doctors table
-    date = models.DateField()  # Current Date
+    date = models.CharField(max_length=100)  # Current Date
     sector = models.CharField(max_length=5)  # West or East
     location = models.CharField(max_length=15)
-    time_in = models.TimeField()
-    daytime_in = models.CharField(max_length=2)  # AM/PM
-    time_out = models.TimeField()
-    daytime_out = models.CharField(max_length=2)  # AM/PM
+    time_in = models.CharField(max_length=10)
+    time_out = models.CharField(max_length=10)
     hours_code = models.CharField(max_length=4)  # FBP/AMCO
-    hours_worked = models.TimeField()  # Calculated from Time in and Time out values
+    hours_worked = models.CharField(max_length=10)  # Calculated from Time in and Time out values
